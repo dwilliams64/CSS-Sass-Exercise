1de97ccc4bda32adf145a729ad450f93311c1d2f
dwilliams64 <dwilliams_64@yahoo.com>
2019-07-25 08:31:57 -0500

    Added debugging js file

---
+++ advance_css_sass/Natours/working/js/debug.js
@@ -0,0 +1,8 @@
+//let nav_chkbox = document.querySelector('.navigation__checkbox');
+//
+//const nav_chk_test = () => {
+//    console.log(nav_chkbox);
+//}
+//
+//nav_chkbox.addEventListener('click', nav_chk_test);
+
